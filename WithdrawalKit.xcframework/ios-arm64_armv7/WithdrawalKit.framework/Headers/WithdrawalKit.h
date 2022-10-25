a5584d9adf73278f06c80721e87fb2a9ef242fd4
Rodrigo Brauwers <rodrigo.brauwers@dock.tech>
2022-10-25 06:47:31 -0300

    [Pod] - Version 2.0.0-beta.3

---
+++ WithdrawalKit.xcframework/ios-arm64_armv7/WithdrawalKit.framework/Headers/WithdrawalKit.h
@@ -0,0 +1,19 @@
+//
+//  WithdrawalKit.h
+//  WithdrawalKit
+//
+//  Created by Denis Wilson de Souza Oliveira on 02/04/20.
+//  Copyright © 2020 Merci. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for WithdrawalKit.
+FOUNDATION_EXPORT double WithdrawalKitVersionNumber;
+
+//! Project version string for WithdrawalKit.
+FOUNDATION_EXPORT const unsigned char WithdrawalKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WithdrawalKit/PublicHeader.h>
+
+
